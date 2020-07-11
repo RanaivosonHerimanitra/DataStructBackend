bb9eae68f9ebfdd81c0525056653cd710c35a15e
Ranaivoson <air_manitra@yahoo.fr>
2020-07-11 18:12:26 +0300

    init-gitignore-Add-DotnetArray-DataStructure

---
+++ Controllers/DotnetArrayController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataStruct;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DataStructBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DotnetArrayController : ControllerBase
+    {
+
+        private readonly ILogger<DotnetArrayController> _logger;
+        private readonly Random _random = new Random();
+
+        public DotnetArrayController(ILogger<DotnetArrayController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetDotnetArrayWithRandomValues/{maxElements}")]
+        public long[] GetDotnetArrayWithRandomValues(int maxElements)
+        {
+            var array = new DotnetArray<long>(maxElements);
+            for (int k = 0; k < maxElements; k++)
+            {
+                array.Insert(_random.Next(0, 100));
+            }
+            return array.toArray();
+        }
+    }
+}
---
+++ DotnetArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStruct
+{
+    public class DotnetArray<T>
+    {
+        private T[] instanceArray;
+        private int nElemens;
+
+        // le constructeur de l Array
+        public DotnetArray(int maxElement)
+        {
+            instanceArray = new T[maxElement];
+            nElemens = 0;
+        }
+
+        public bool Find(T searchKey)
+        {
+            int j;
+            for (j = 0; j < nElemens; j++)
+            {
+                if (instanceArray[j].Equals(searchKey))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Insert(T value)
+        {
+            instanceArray[nElemens] = value;
+            nElemens += 1;
+        }
+
+        public bool Delete(T value)
+        {
+            int j;
+            for (j = 0; j < nElemens; j++)
+            {
+                if (instanceArray[j].Equals(value))
+                {
+                    // rempli l espace memoire vide
+                    // decalage a gauche
+                    // value at k th will be erased
+                    for (int k = j; k < nElemens; k++)
+                    {
+                        instanceArray[k] = instanceArray[k + 1];
+                    }
+                    // decremente le nbre totql d element car on a perdu 1:
+                    nElemens -= 1;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public T[] toArray()
+        {
+            return instanceArray as T[];
+        }
+    }
+}
