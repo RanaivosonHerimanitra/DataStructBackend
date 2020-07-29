400e3ec7e5392ecabebb50c1b5d6522fe4985811
Ranaivoson <air_manitra@yahoo.fr>
2020-07-29 18:14:35 +0300

    simple-linkedlist-implementation

---
+++ DotnetLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Threading.Tasks;
+
+namespace DataStructBackend
+{
+    /*
+     * Linked list with simple operation:
+     * inserting an item at the beginning of the list
+     * deleting an item at the beginning of the list
+     */
+    public class DotnetLinkedList
+    {
+        public int iData;
+        public decimal dData;
+        public DotnetLinkedList first;
+        public DotnetLinkedList next;
+
+        public DotnetLinkedList(int id, decimal value)
+        {
+            iData = id;
+            dData = value;
+        }
+
+        public bool IsEmpty()
+        {
+            return first == null;
+        }
+
+        public void InsertFirst(int id, decimal value)
+        {
+            DotnetLinkedList newLink = new DotnetLinkedList(id, value);
+            newLink.next = first;
+            first = newLink;
+        }
+
+        public void DeleteFirst()
+        {
+            first = first.next;
+        }
+    }
+}
